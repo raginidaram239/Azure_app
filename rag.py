82277eb0bd7fc556c8009006dd31b393cb3a5640
raginid <Ragini_Daram@ajuserv.com>
2024-04-10 17:47:01 +0530

    Add files from Azure_app directory

--- rag.py
+++ rag.py
@@ -7,7 +7,7 @@ st.title("Mortgage Repayments Calculator")
 
 st.write("### Input Data")
 col1, col2 = st.columns(2)
-home_value = col1.number_input("Home Value", min_value=0, value=500000)
+home_value = col1.number_input("House", min_value=0, value=500000)
 deposit = col1.number_input("Deposit", min_value=0, value=100000)
 interest_rate = col2.number_input("Interest Rate (in %)", min_value=0.0, value=5.5)
 loan_term = col2.number_input("Loan Term (in years)", min_value=1, value=30)
